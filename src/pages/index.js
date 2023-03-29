680d1332b5cf0c1cb3eccd5e23c78b68e457d4e5
maitrakhatri <maitrakhatri@gmail.com>
2023-03-29 17:07:41 +0530

    document title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import Tiptap from "@/components/TipTap";
-
+import Head from "next/head";
 export default function Home() {
-  // document.title = "TipTap";
   return (
     <>
+      <Head>
+        <title>TipTap | Maitra Khatri</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <h1 style={{ margin: "2rem" }}>Playing around with TipTap</h1>
       <Tiptap />
     </>
